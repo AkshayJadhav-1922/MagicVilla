ceffc42e2b94d148639a9c5b2f9896e290807ea5
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-08 13:22:20 +0530

    Added First Controller and API Endpoint

---
+++ WebApplication1/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    //if we are using mvc, it will have Controller instead of Controller base
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>()
+            {
+                new Villa() {Id=1 , Name="Pool Villa"},
+                new Villa() {Id=2 , Name="Beach Villa"}
+            };
+        }
+    }
+}
