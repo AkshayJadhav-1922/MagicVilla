cde7875a60a0afe23d30b3cdd6848b05bc8de15b
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-08 19:42:38 +0530

    Added First DTO

---
+++ WebApplication1/Models/Dto/VillaDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models.Dto
+{
+    public class VillaDTO
+    {
+        //DTOs provide a wrapper between entities and database models and what is being exposed from the api
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
--- WebApplication1/Models/Villa.cs
+++ WebApplication1/Models/Villa.cs
@@ -1,8 +1,11 @@
-﻿namespace WebApplication1.Models
+﻿using System.Data;
+
+namespace WebApplication1.Models
 {
     public class Villa
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
     }
 }
