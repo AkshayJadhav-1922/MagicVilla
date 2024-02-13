cc611c7a30ef0b18b803223b44464da3f1c99115
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-13 11:38:06 +0530

    Created and Implemented IVillNumberRepository

---
+++ WebApplication1/Repository/VillaNumberRepository.cs
@@ -0,0 +1,27 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class VillaNumberRepository : Repository<VillaNumber>, IVillaNumberRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaNumberRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+        
+        public async Task<VillaNumber> UpdateAsync(VillaNumber entiy)
+        {
+            entiy.UpdatedDate = DateTime.Now;
+            _db.VillaNumbers.Update(entiy);
+            await SaveAsync();
+            return entiy;
+        }
+
+    }
+}
