e88cddb66929244e0f4baa8daa0ed8b38b3c90a0
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-14 22:57:05 +0530

    Added VillNumber Controller and Index action method with view

---
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly IVillaNumberService _villaNumberService;
+        public VillaNumberController(IMapper mapper, IVillaNumberService villaNumberService)
+        {
+            _mapper = mapper;
+            _villaNumberService = villaNumberService;
+        }
+        public async Task<ActionResult> IndexVillaNumber()
+        {
+            List<VillaNumberDTO> list = new();
+            var response = await _villaNumberService.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaNumberDTO>>(Convert.ToString(response.Result));
+            }
+            return View(list);
+        }
+
+      
+    }
+}
