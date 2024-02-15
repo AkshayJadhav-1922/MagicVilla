58e56549d0e5b3314016dedd73f80c4a8db0e290
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-15 13:34:08 +0530

    Created ViewModel for VillNumberCreate functionality. Also configured Villa dropdown data

--- MagicVilla_Web/Controllers/VillaNumberController.cs
+++ MagicVilla_Web/Controllers/VillaNumberController.cs
@@ -1,9 +1,11 @@
 ﻿using AutoMapper;
 using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Models.VM;
 using MagicVilla_Web.Services;
 using MagicVilla_Web.Services.IServices;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers
@@ -12,10 +14,12 @@ namespace MagicVilla_Web.Controllers
     {
         private readonly IMapper _mapper;
         private readonly IVillaNumberService _villaNumberService;
-        public VillaNumberController(IMapper mapper, IVillaNumberService villaNumberService)
+        private readonly IVillaService _villaService;
+        public VillaNumberController(IMapper mapper, IVillaNumberService villaNumberService, IVillaService villaService)
         {
             _mapper = mapper;
             _villaNumberService = villaNumberService;
+            _villaService = villaService;
         }
         public async Task<ActionResult> IndexVillaNumber()
         {
@@ -28,6 +32,38 @@ namespace MagicVilla_Web.Controllers
             return View(list);
         }
 
-      
+        public async Task<ActionResult> CreateVillaNumber()
+        {
+            VillaNumberCreateVM villaNumberCreateVM = new VillaNumberCreateVM();
+
+            var response = await _villaService.GetAllAsycn<APIResponse>();
+            if (response != null && response.IsSuccess)
+            {
+                villaNumberCreateVM.villaList = JsonConvert.DeserializeObject<List<VillaDTO>>
+                    (Convert.ToString(response.Result)).Select(i => new SelectListItem
+                    {
+                        Text = i.Name,
+                        Value = i.Id.ToString()
+                    });
+            }
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CreateVillaNumber(VillaNumberCreateVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _villaNumberService.CreateAsync<APIResponse>(model.villaNumber);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(IndexVillaNumber));
+                }
+            }
+            return View(model);
+        }
+
+
     }
 }
