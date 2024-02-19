e212374dc9b9c1e7f1937417ea0af39b3a6f6a29
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-19 18:58:45 +0530

    Completed Implementation of Login Method with JWT tocken generation

--- WebApplication1/Repository/UserRepository.cs
+++ WebApplication1/Repository/UserRepository.cs
@@ -2,6 +2,10 @@
 using MagicVilla_VillaAPI.Models.Dto;
 using MagicVilla_VillaAPI.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using WebApplication1.Data;
 
 namespace MagicVilla_VillaAPI.Repository
@@ -26,18 +30,40 @@ namespace MagicVilla_VillaAPI.Repository
             return false;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            var user = _db.LocalUsers.FirstOrDefaultAsync(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower() 
-            && u.Password.ToLower()== loginRequestDTO.Password.ToLower());
-            
+            var user = await _db.LocalUsers.FirstOrDefaultAsync(u => u.UserName.ToLower() == loginRequestDTO.UserName.ToLower()
+            && u.Password.ToLower() == loginRequestDTO.Password.ToLower());
+
             if (user == null)
             {
                 return null;
             }
 
-            //generating JWT tocken
+            //if user was found, generate JWT Tocken
+            var tockenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
 
+            var tockenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var tocken = tockenHandler.CreateToken(tockenDescriptor);
+
+            LoginResponseDTO loginResponseDTO = new()
+            {
+                Tocken = tockenHandler.WriteToken(tocken),
+                User = user
+            };
+
+            return loginResponseDTO;
 
         }
 
