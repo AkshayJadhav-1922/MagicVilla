b1b3f776223e1323e49714d2065fc4a08b0803c3
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-19 18:31:41 +0530

    Added Userrepository and Implemeted Register and IsUniqueUsername methods

---
+++ WebApplication1/Repository/UserRepository.cs
@@ -0,0 +1,46 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using WebApplication1.Data;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool IsUniqueUser(string username)
+        {
+            var user = _db.LocalUsers.FirstOrDefault(u => u.UserName == username);
+            if (user == null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<LocalUser> Register(RegitrationRequestDTO regitrationRequestDTO)
+        {
+            LocalUser user = new()
+            {
+                UserName = regitrationRequestDTO.UserName,
+                Password = regitrationRequestDTO.Password,
+                Name = regitrationRequestDTO.Name,
+                Role = regitrationRequestDTO.Role
+            };
+
+            await _db.LocalUsers.AddAsync(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
+        }
+    }
+}
