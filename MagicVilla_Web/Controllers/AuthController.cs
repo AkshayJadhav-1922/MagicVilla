1bd57cdfaa05d8df7ea2c1921cb3e2346cf86c29
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-20 13:12:18 +0530

    Configured Session to store tocken value and written Login and Logout methods

--- MagicVilla_Web/Controllers/AuthController.cs
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
-﻿using MagicVilla_Web.Models;
+﻿using MagicVilla_Utility;
+using MagicVilla_Web.Models;
 using MagicVilla_Web.Models.Dto;
 using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace MagicVilla_Web.Controllers
 {
@@ -24,7 +27,19 @@ namespace MagicVilla_Web.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginRequestDTO obj)
         {
-            return View(obj);
+            APIResponse response = await _authService.LogInAsync<APIResponse>(obj);
+            if(response!= null && response.IsSuccess)
+            {
+                LoginResponseDTO result = JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(response.Result));
+                HttpContext.Session.SetString(SD.SessionTocken, result.Tocken);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("CustomError", response.ErrorMessages.FirstOrDefault());
+                return View(obj);
+
+            }
         }
 
         [HttpGet]
@@ -47,7 +62,9 @@ namespace MagicVilla_Web.Controllers
 
         public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            HttpContext.Session.SetString(SD.SessionTocken, "");
+            return RedirectToAction("Index", "Home");
         }
 
         public IActionResult AccessDenied()
