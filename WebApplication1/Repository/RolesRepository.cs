6092bc8b24ea63b7c1e432f29defe87c36b16793
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-20 20:50:19 +0530

    Created and Implimented RolesRepository

---
+++ WebApplication1/Repository/RolesRepository.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+
+namespace MagicVilla_VillaAPI.Repository
+{
+    public class RolesRepository : IRolesRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public RolesRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IEnumerable<Role> GetAllRole()
+        {
+            return _db.Roles.ToList();
+        }
+    }
+}
