f07582a7588f96bd8424c7d7c46eb8ea35879fb4
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-20 12:39:52 +0530

    Added AuthController with required action methods

---
+++ MagicVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,59 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            authService = _authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View(obj);
+        }
+
+        [HttpGet]
+        public IActionResult Register() 
+        { 
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegitrationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if(result != null && result.IsSuccess)
+            {
+                RedirectToAction("Login");
+            }
+            return View(obj);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
